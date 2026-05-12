4f8873d04cc1aa9dd633e657e92e327409274905
leonovalexander212 <ravenx2k17@.com>
2026-05-12 09:14:00 +0700

    fix: correct dist path in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'preproom-server',
-      script: './server/dist/index.js',
+      script: './server/dist/src/index.js',
       instances: 1,
       exec_mode: 'fork',
       env: {
